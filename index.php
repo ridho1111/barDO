1ec6ce322a26badb66928a3b50981d125591f969
ridho1111 <ridho.180119@gmail.com>
2020-04-23 08:28:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>barDO</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
+	  <div class="container">
+		  	<a class="navbar-brand" href="#">barDO</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav">
+		      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+		    </div>
+		  </div>
+	  </div>
+  </nav>	
+ 
+  <div class="container">
+  	<div class="row mt-3">
+  		<div class="col">
+  			<h1>All Menu</h1>
+  		</div>
+  	</div>
+
+  	<div class="row">
+  		<div class="col-md-4">
+  			<div class="card"">
+			  <img src="img/menu/sate ayam.png" class="card-img-top">
+			  <div class="card-body">
+			    <h5 class="card-title">Sate Ayam</h5>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			    <h5>Rp.17.000</h5>
+			    <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+  				</div>
+				</div>
+  			</div>
+  		</div>
+  	</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html> 
